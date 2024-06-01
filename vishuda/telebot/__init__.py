b9caaf12f9af661a15c9f7e7f46b3a0f345e10ef
Cedric Roman <roman@numengo.com>
2024-06-02 00:55:00 +0100

    telebot progress

--- vishuda/telebot/__init__.py
+++ vishuda/telebot/__init__.py
@@ -1,3 +1,68 @@
+from typing import Type, cast, Dict, Any
+
+from aiogram_forms.forms import FormsManager
+from aiogram_forms.forms.base import Field, Form
+from aiogram_forms.core.entities import Entity
+from aiogram_forms import utils
+
+
+async def show(self, name: str) -> None:
+    entity_container: Type['Form'] = self._get_form_by_name(name)
+    data = await self.state.get_data()
+    for inherited_form, required in getattr(entity_container, 'inherited_forms_required', []):
+        form_data = data.get(inherited_form)
+        if not form_data and required:
+            await self.show(inherited_form)
+    states = entity_container.state.get_states()
+    if states:
+        first_entity = cast(Field, states[0].entity)
+        await self.state.set_state(first_entity.state)
+        await self.event.answer(str(first_entity.label), reply_markup=first_entity.reply_keyboard)  # type: ignore[arg-type]
+    else:
+        #await self.state.set_state(None)
+        await entity_container.callback(self.event, **self.data)
+
+
+async def get_data(self, name: str) -> Dict[str, Any]:
+    entity_container: Type['Form'] = self._get_form_by_name(name)
+    data = await self.state.get_data()
+    form_data = data.get(entity_container.__name__)
+    if not form_data or not isinstance(form_data, dict):
+        form_data = {}
+    for inherited_form, required in getattr(entity_container, 'inherited_forms_required', []):
+        if required:
+            entity_container: Type['Form'] = self._get_form_by_name(inherited_form)
+            inherited_form_data = data.get(entity_container.__name__)
+            if not inherited_form_data or not isinstance(inherited_form_data, dict):
+                inherited_form_data = {}
+            form_data.update(inherited_form_data)
+    return form_data
+
+
+async def get_data_print(self, name: str) -> Dict[str, Any]:
+    data = await self.get_data(name)
+    labels = {}
+    entity_container: Type['Form'] = self._get_form_by_name(name)
+    form_fields = utils.get_attrs_of_type(entity_container, Entity)
+    for fn, field in form_fields:
+        labels[fn] = field.label
+        if fn in data and field.__class__.__name__ == 'ChoiceField':
+            data[fn] = {c[1]: c[0] for c in getattr(entity_container, fn).choices}.get(data[fn])
+
+    for inherited_form, required in getattr(entity_container, 'inherited_forms_required', []):
+        entity_container: Type['Form'] = self._get_form_by_name(inherited_form)
+        form_fields = utils.get_attrs_of_type(entity_container, Entity)
+        for fn, field in form_fields:
+            labels[fn] = field.label
+            if fn in data and field.__class__.__name__ == 'ChoiceField':
+                data[fn] = {c[1]: c[0] for c in getattr(entity_container, fn).choices}.get(data[fn])
+    return {labels.get(k, k): v  for k, v in data.items()}
+
+
+FormsManager.show = show
+FormsManager.get_data = get_data
+FormsManager.get_data_print = get_data_print
+
 
 def create_buttons_menu(buttons=[], n_cols=1, header_buttons=[], footer_buttons=[]):
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
@@ -18,6 +83,8 @@ def create_lang_buttons(languages, state_name='user_lang'):
 
 
 __all__ = [
+    'FormsManager',
+    'Form',
     'create_buttons_menu',
     'create_lang_buttons',
 ]
--- vishuda/telebot/fields.py
+++ vishuda/telebot/fields.py
@@ -1,9 +1,27 @@
 from aiogram_forms.forms.fields import (
-    TextField,
-    EmailField,
-    PhoneNumberField,
-    ChoiceField
+    TextField as TextField_required,
+    EmailField as EmailField_required,
+    PhoneNumberField as PhoneNumberField_required,
+    ChoiceField as ChoiceField_required
 )
+from aiogram_forms.forms.base import Field
+
+
+class OptionalField(Field):
+    """Field implementation with `reauired` attribute """
+    required = True
+    default = None
+
+    def __init__(self, *args, required=True, default=None, **kwargs):
+        super().__init__(*args, **kwargs)  # type: ignore[arg-type]
+        self.required = required
+        self.default = default
+
+
+TextField = type('TextField', (OptionalField, TextField_required), {})
+EmailField = type('EmailField', (OptionalField, EmailField_required), {})
+PhoneNumberField = type('PhoneNumberField', (OptionalField, PhoneNumberField_required), {})
+ChoiceField = type('ChoiceField', (OptionalField, ChoiceField_required), {})
 
 
 __all__ = [
